23245e4955ee3d7058ec733b6865b144cb0c3f6b
issammanhub <48328165+issammanhub@users.noreply.github.com>
2024-12-21 18:23:12 +0100

    Create start.bat

---
+++ resources/start.bat
@@ -0,0 +1,22 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By Aissam" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user Aissam l7aj_jab_l3cha /add >nul
+net localgroup administrators Aissam /add >nul
+net user Aissam /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant Aissam:F >nul
+ICACLS C:\Windows\installer /grant Aissam:F >nul
+echo Successfully installed! If RDP is dead, rebuild again.
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: Aissam
+echo Password: l7aj_jab_l3cha
+echo You can login now
+echo RDP By @Aissam
+ping -n 10 127.0.0.1 >nul
